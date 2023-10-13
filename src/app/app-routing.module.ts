f902e0bd1044bb249f6c6000f5c1bd3ddc8cce78
AdnanAbbas199 <aabbas@avanceon.ae>
2023-10-13 10:22:58 +0500

    CRUD DONE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataInformationComponent } from './data-information/data-information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'DataInformation',component:DataInformationComponent}
+  // {path:'',component:DataInformationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
